cda9bb9e32c732d9bf305e4004fd8a139ab4a850
Hyojeong Yu <40685291+hjyu94@users.noreply.github.com>
2022-01-06 23:18:00 +0900

    Add: eureka client setting files (#2)
    Co-authored-by: Hyojeong-Yu <hjeong@lunit.io>

--- src/main/kotlin/com/toring/userservice/UserServiceApplication.kt
+++ src/main/kotlin/com/toring/userservice/UserServiceApplication.kt
@@ -2,7 +2,9 @@ package com.toring.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class UserServiceApplication
 
